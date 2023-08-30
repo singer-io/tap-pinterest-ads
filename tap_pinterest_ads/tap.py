efcee5fa61f3a6f3f837950cc14119f6c53f6faa
Gary James <gary.james19@gmail.com>
2023-08-30 16:32:35 +0100

    fix - Config JsonSchema on Tap Class

--- tap_pinterest_ads/tap.py
+++ tap_pinterest_ads/tap.py
@@ -22,42 +22,43 @@ STREAM_TYPES = [
     AccountAnalyticsStream
 ]
 
-CONFIG = th.PropertiesList(
-    th.Property(
-        "client_id",
-        th.StringType,
-        required=True,
-        description="App ID"
-    ),
-    th.Property(
-        "client_secret",
-        th.StringType,
-        required=True,
-        description="App secret key"
-    ),
-    th.Property(
-        "refresh_token",
-        th.StringType,
-        required=True,
-        description="Refresh token obtained from the OAuth user flow"
-    ),
-    th.Property(
-        "start_date",
-        th.DateTimeType,
-        default="2019-10-17T00:00:00Z",
-        description="Date to start collection analytics from"
-    ),
-    th.Property(
-        "is_backfilled",
-        th.BooleanType,
-        default=False,
-        description="Set to True once backfilled in order to reduce API calls per day"
-    ),
-).to_dict()
 
 class TapPinterestAds(Tap):
     """pinterest tap class."""
     name = "tap-pinterest-ads"
+
+    config_jsonschema = th.PropertiesList(
+        th.Property(
+            "client_id",
+            th.StringType,
+            required=True,
+            description="App ID"
+        ),
+        th.Property(
+            "client_secret",
+            th.StringType,
+            required=True,
+            description="App secret key"
+        ),
+        th.Property(
+            "refresh_token",
+            th.StringType,
+            required=True,
+            description="Refresh token obtained from the OAuth user flow"
+        ),
+        th.Property(
+            "start_date",
+            th.DateTimeType,
+            default="2019-10-17T00:00:00Z",
+            description="Date to start collection analytics from"
+        ),
+        th.Property(
+            "is_backfilled",
+            th.BooleanType,
+            default=False,
+            description="Set to True once backfilled in order to reduce API calls per day"
+        ),
+    ).to_dict()
 
     def discover_streams(self) -> List[Stream]:
         """Return a list of discovered streams."""
